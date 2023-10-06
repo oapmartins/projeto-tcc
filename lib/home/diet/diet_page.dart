239a191659987bf84883be9e449a1469c3c8fa7b
Octavio <otavio@bodyfitteam.com.br>
2023-10-06 08:03:10 -0300

    Adicionando página de consulta

--- lib/home/diet/diet_page.dart
+++ lib/home/diet/diet_page.dart
@@ -24,147 +24,192 @@ class _DietPageState extends State<DietPage> {
           builder: (controller) {
             return ListView(
               padding: const EdgeInsets.all(16),
-              children: [
-                Column(
-                  children: [
-                    ExpansionTile(
-                      tilePadding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                      backgroundColor: Colors.green.shade50,
-                      collapsedShape: const ContinuousRectangleBorder(
-                        side: BorderSide(
-                          color: Colors.green,
-                        ),
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                        ),
-                      ),
-                      shape: const ContinuousRectangleBorder(
-                        side: BorderSide(
-                          color: Colors.green,
-                        ),
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                        ),
-                      ),
-                      // backgroundColor: Colors.yellow,
-                      collapsedBackgroundColor: Colors.grey.shade200,
-                      collapsedIconColor: Colors.green,
-                      iconColor: Colors.green,
-                      title: const Row(
-                        children: [
-                          Icon(FontAwesomeIcons.breadSlice),
-                          SizedBox(width: 15),
-                          Text(
-                            'Café da manhã',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ],
-                      ),
-                      children: const [
-                        ListTile(
-                          shape: ContinuousRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(20),
-                              topRight: Radius.circular(20),
-                            ),
-                          ),
-                          title: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Column(
-                                    children: [
-                                      Text('2 unidades'),
-                                    ],
-                                  ),
-                                  Column(
-                                    children: [
-                                      Text('Ovos'),
-                                    ],
-                                  ),
-                                  Column(
-                                    children: [
-                                      Text('100 Kcal'),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                              Divider(),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Column(
-                                    children: [
-                                      Text('2 fatias'),
-                                    ],
-                                  ),
-                                  Column(
-                                    children: [
-                                      Text('Pão'),
-                                    ],
-                                  ),
-                                  Column(
-                                    children: [
-                                      Text('30 Kcal'),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                              Divider(),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Column(
-                                    children: [
-                                      Text('1 copo'),
-                                    ],
-                                  ),
-                                  Column(
-                                    children: [
-                                      Text('Café puro'),
-                                    ],
-                                  ),
-                                  Column(
-                                    children: [
-                                      Text('10 Kcal'),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      width: Get.size.width,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: Colors.black26,
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(20),
-                          bottomRight: Radius.circular(20),
-                        ),
-                        border: Border.all(
-                          color: Colors.green,
-                        ),
-                      ),
-                      child: Center(
-                        child: Text('Adicionar Alimentos'),
-                      ),
-                    )
-                  ],
-                ),
+              children: const [
+                DietComponentWidget(title: 'Café da manhã'),
+                DietComponentWidget(title: 'Almoço'),
+                DietComponentWidget(title: 'Café da tarde'),
+                DietComponentWidget(title: 'Jantar'),
               ],
             );
           },
         ),
       ),
+    );
+  }
+}
+
+class DietComponentWidget extends StatelessWidget {
+  const DietComponentWidget({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ExpansionTile(
+          tilePadding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          backgroundColor: Colors.green.shade50,
+          collapsedShape: const ContinuousRectangleBorder(
+            side: BorderSide(
+              color: Colors.green,
+            ),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          shape: const ContinuousRectangleBorder(
+            side: BorderSide(
+              color: Colors.green,
+            ),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          // backgroundColor: Colors.yellow,
+          collapsedBackgroundColor: Colors.green.shade50,
+          collapsedIconColor: Colors.green,
+          iconColor: Colors.green,
+          title: Row(
+            children: [
+              Icon(FontAwesomeIcons.breadSlice),
+              SizedBox(width: 15),
+              Text(
+                title,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          children: const [
+            ListTile(
+              shape: ContinuousRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              title: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text('2 unidades'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('Ovos'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('100 Kcal'),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Divider(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text('2 fatias'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('Pão'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('30 Kcal'),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Divider(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text('1 copo'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('Café puro'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('10 Kcal'),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Divider(color: Colors.black),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            'Total',
+                            style: TextStyle(fontWeight: FontWeight.w800),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            '140 Kcal',
+                            style: TextStyle(fontWeight: FontWeight.w800),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        GestureDetector(
+          onTap: () {
+            Get.toNamed('/search-food');
+          },
+          child: Container(
+            width: Get.size.width,
+            height: 40,
+            decoration: BoxDecoration(
+              color: Colors.green.shade200,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+              border: Border.all(
+                color: Colors.green,
+              ),
+            ),
+            child: Center(
+              child: Text('Adicionar Alimentos'),
+            ),
+          ),
+        ),
+        SizedBox(height: 15),
+      ],
     );
   }
 }
