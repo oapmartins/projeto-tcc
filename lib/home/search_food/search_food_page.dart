b8721c42a3228a69a414adc315414b9324621352
Octavio <otavio@bodyfitteam.com.br>
2023-10-09 22:36:02 -0300

    Adicionando tela de busca de alientos

--- lib/home/search_food/search_food_page.dart
+++ lib/home/search_food/search_food_page.dart
@@ -1,15 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchFoodPage extends StatelessWidget {
   const SearchFoodPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Adicione seu alimento'),
+    return SafeArea(
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              toolbarHeight: 100,
+              title: SearchBar(
+                padding: const MaterialStatePropertyAll<EdgeInsets>(
+                  EdgeInsets.symmetric(horizontal: 16.0),
+                ),
+                elevation: const MaterialStatePropertyAll(1),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    side: const BorderSide(
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
+                leading: const Icon(
+                  Icons.search,
+                  color: Colors.green,
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate((context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: const Column(
+                      children: [
+                        Text('Contra Filé'),
+                        Text('178 cal, 100 g, Friboi'),
+                      ],
+                    ),
+                    trailing: const Icon(FontAwesomeIcons.circlePlus),
+                    tileColor: Colors.grey.shade200,
+                  ),
+                );
+              }),
+            ),
+          ],
+        ),
       ),
-      body: Container(),
     );
   }
 }
